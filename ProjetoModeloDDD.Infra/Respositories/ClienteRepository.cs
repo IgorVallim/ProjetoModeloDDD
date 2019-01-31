9799308d57e774386254a283c11f1a95afd3e9a6
Igor Vallim <igor.vallim@corp.evosystems.com.br>
2019-01-31 14:42:18 -0200

    Cricacao do controller de cliente

---
+++ ProjetoModeloDDD.Infra/Respositories/ClienteRepository.cs
@@ -0,0 +1,10 @@
+﻿using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.Domain.Interfaces.Repositories;
+
+namespace ProjetoModeloDDD.Infra.Respositories
+{
+    public class ClienteRepository : RepositoryBase<Cliente>, IClienteRepository
+    {
+
+    }
+}
